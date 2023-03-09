4f7c7273a30c73a250fcf66aae0501f871b60e69
khanhnhanbk <nhanhoa21012002@gmail.com>
2023-03-09 09:37:38 +0700

    change logo

--- app/views/layouts/layout.php
+++ app/views/layouts/layout.php
@@ -5,12 +5,12 @@
   <meta charset="utf-8">
   <meta content="width=device-width, initial-scale=1.0" name="viewport">
 
-  <title>Rapid Bootstrap Template - Index</title>
+  <title>LearnHub</title>
   <meta content="" name="description">
   <meta content="" name="keywords">
 
   <!-- Favicons -->
-  <link href="<?=PATH_URL?>images/favicon.png" rel="icon">
+  <link href="<?=PATH_URL?>images/logo.png" rel="icon">
   <link href="<?=PATH_URL?>images/apple-touch-icon.png" rel="apple-touch-icon">
 
   <!-- Google Fonts -->
@@ -40,9 +40,10 @@
   <header id="header" class="fixed-top d-flex align-items-center header-transparent">
     <div class="container d-flex align-items-center">
 
-      <h1 class="logo me-auto"><a href="index.html">Rapid</a></h1>
-      <!-- Uncomment below if you prefer to use an image logo -->
-      <!-- <a href="index.html" class="logo me-auto"><img src="<?=PATH_URL?>images/logo.png" alt="" class="img-fluid"></a>-->
+      <h1 class="logo me-auto">
+        <!-- Uncomment below if you prefer to use an image logo -->
+        <a href="index.html" class="logo me-auto"><img src="<?=PATH_URL?>images/logo.png" alt="" class="img-fluid"></a>
+      </h1>
       <?php
       include('navbar.php');
       ?>
