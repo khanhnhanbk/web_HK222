ecbfebc4686e09d570bbe94ea81db8d6837ad37f
Anh Kiet <ct.winak11@gmail.com>
2023-04-20 14:57:08 +0700

    adjust footer setting

--- app/views/layouts/inc/footer.php
+++ app/views/layouts/inc/footer.php
@@ -12,7 +12,7 @@
                             <div class="col-sm-6">
 
                                 <div class="footer-info">
-                                    <h3>Rapid</h3>
+                                    <h3><?= $setting['logo_url'] ?></h3>
                                     <p>Cras fermentum odio eu feugiat lide par naso tierra. Justo eget nada terra videa magna derita valies darta donna mare fermentum iaculis eu non diam phasellus. Scelerisque felis imperdiet proin fermentum leo. Amet volutpat consequat mauris nunc congue.</p>
                                 </div>
 
@@ -101,7 +101,7 @@
 
         <div class="container">
             <div class="copyright">
-                &copy; Copyright <strong>Rapid</strong>. All Rights Reserved
+                &copy; Copyright <strong><?= $setting['logo_url'] ?></strong>. All Rights Reserved
             </div>
             <div class="credits">
                 <!--
