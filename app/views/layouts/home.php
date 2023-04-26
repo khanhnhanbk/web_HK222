80e3a5022eb36d017046cb6609b351c3b0377cd4
vinh2110 <vinh.nguyendinh@hcmut.edu.vn>
2023-04-26 23:13:26 +0700

    edit navbar

--- app/views/layouts/home.php
+++ app/views/layouts/home.php
@@ -19,10 +19,7 @@ include_once(USER_PATH . 'views/layouts/inc/hero.php');
     <section id="breadcrumbs" class="breadcrumbs">
         <div class="container">
 
-            <ol>
-                <li><a href="index.html">Home</a></li>
-                <li>Authorize</li>
-            </ol>
+            
 
 
             <section class="content inner-page pt-4">
